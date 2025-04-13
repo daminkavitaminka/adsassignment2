0d125d50a3f9ffb6e61278ae1024ad3549a07663
damelitaubasar <taubasar.d@gmail.com>
2025-04-13 23:33:11 +0500

    .

---
+++ src/MyStack.java
@@ -0,0 +1,31 @@
+public class MyStack<T> {
+    private MyArrayList<T> list;
+
+    public MyStack() {
+        list = new MyArrayList<>();
+    }
+
+    public void push(T element) {
+        list.add(element);
+    }
+
+    public T pop() {
+        if (list.isEmpty()) throw new IllegalStateException("Stack is empty");
+        T element = list.get(list.size() - 1);
+        list.remove(list.size() - 1);
+        return element;
+    }
+
+    public T peek() {
+        if (list.isEmpty()) throw new IllegalStateException("Stack is empty");
+        return list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
+        return list.size();
+    }
+}
