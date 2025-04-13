486a6d518d47ab3eb911b430a6a241eb20be17b1
damelitaubasar <taubasar.d@gmail.com>
2025-04-13 23:29:15 +0500

    myarraylist

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -7,42 +7,5 @@ public class MyArrayList<T> implements MyList<T> {
         size = 0;
     }
 
-    @Override
-    public void add(T element) {
-        if (size == elements.length) {
-            resize();
-        }
-        elements[size++] = element;
-    }
 
-    @Override
-    public T get(int index) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        return (T) elements[index];
-    }
-
-    @Override
-    public void remove(int index) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        for (int i = index; i < size - 1; i++) {
-            elements[i] = elements[i + 1];
-        }
-        elements[--size] = null; // Prevent memory leak
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    private void resize() {
-        Object[] newElements = new Object[elements.length * 2];
-        System.arraycopy(elements, 0, newElements, 0, elements.length);
-        elements = newElements;
-    }
 }
